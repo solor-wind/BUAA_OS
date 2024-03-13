c31af1d440a1a0d30d0b5f7bd1cdf0278968f804
高鹏飞 <22373074@buaa.edu.cn>
2024-03-13 21:26:01 +0800

    initial

---
+++ code/0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void output1();
+void output2();
+void output3();
+void output4();
+void output5();
+void output6();
+void output7();
+void output8();
+void output9();
+void output10();
+void output11();
+void output12();
+void output13();
+void output14();
+void output15();
+int main() {
+  output8();
+  output3();
+  output6();
+  output12();
+  output4();
+  output11();
+  output13();
+  output14();
+  output15();
+  output5();
+  output1();
+  output2();
+  output10();
+  output7();
+  output9();
+  return 0;
+}
--- handle-ps.sh
+++ handle-ps.sh
@@ -34,10 +34,10 @@ fi
 # You can remove ":" after finishing.
 if $SORT; then
     # Your code here. (1/3)
-    :
+    sort -k4nr -k2n $FILE
 elif [ ! -z "$CMD" ]; then
     # Your code here. (2/3)
-    :
+    awk "$CMD {print $1,$2,$3,$4}" $FILE
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
     :
