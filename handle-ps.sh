0df9eca865d7314a06e15da3c645f516f956e2b1
高鹏飞 <22373074@buaa.edu.cn>
2024-03-13 21:29:48 +0800

    3

--- handle-ps.sh
+++ handle-ps.sh
@@ -37,7 +37,7 @@ if $SORT; then
     sort -k4nr -k2n $FILE
 elif [ ! -z "$CMD" ]; then
     # Your code here. (2/3)
-    awk "$CMD {print \$1,\$2,\$3,\$4,\$5}" $FILE
+    awk "[$CMD] {print \$1,\$2,\$3,\$4,\$5}" $FILE
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
     :
