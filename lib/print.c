844cf2cf44182676052475263d1cc1cf3f3651cb
高鹏飞 <22373074@buaa.edu.cn>
2024-03-14 21:04:37 +0800

    1_2

--- lib/print.c
+++ lib/print.c
@@ -19,24 +19,47 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 	for (;;) {
 		/* scan for the next '%' */
 		/* Exercise 1.4: Your code here. (1/8) */
-
+		char* next=fmt;
+		while (*next!='%'&&*next!='\0')
+			next++;
 		/* flush the string found so far */
 		/* Exercise 1.4: Your code here. (2/8) */
-
+		out(data,fmt,next-fmt);
+		fmt=next;
 		/* check "are we hitting the end?" */
 		/* Exercise 1.4: Your code here. (3/8) */
-
+		if(*fmt=='\0')
+			break;
 		/* we found a '%' */
 		/* Exercise 1.4: Your code here. (4/8) */
-
+		fmt++;
 		/* check format flag */
 		/* Exercise 1.4: Your code here. (5/8) */
-
+		ladjust=0;
+		padc=' ';
+		while(*fmt=='-'||*fmt=='0')
+		{
+			if(*fmt=='-')
+				ladjust=1;
+			if(*fmt=='0')
+				padc='0';
+			fmt++;
+		}
 		/* get width */
 		/* Exercise 1.4: Your code here. (6/8) */
-
+		width=0;
+		while(*fmt>='0'&&*fmt<=9)
+		{
+			width*=10;
+			width+=*fmt-'0';
+			fmt++;
+		}
 		/* check for long */
 		/* Exercise 1.4: Your code here. (7/8) */
+		long_flag=0;
+		if(*fmt=='l')
+			long_flag=1;
+		fmt++;
 
 		neg_flag = 0;
 		switch (*fmt) {
@@ -56,7 +79,9 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			} else {
 				num = va_arg(ap, int);
 			}
-
+			if(num<0)
+				neg_flag=1;
+			print_num(out, data, num, 10, neg_flag, width, ladjust, padc, 0);
 			/*
 			 * Refer to other parts (case 'b', case 'o', etc.) and func 'print_num' to
 			 * complete this part. Think the differences between case 'd' and the
