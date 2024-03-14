2ccdbe219d0210d2dbb36af5b4fc8d124d31d7a2
高鹏飞 <22373074@buaa.edu.cn>
2024-03-14 21:31:41 +0800

    jal

--- lib/print.c
+++ lib/print.c
@@ -19,7 +19,7 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 	for (;;) {
 		/* scan for the next '%' */
 		/* Exercise 1.4: Your code here. (1/8) */
-		char* next=fmt;
+		char* next=(char *)fmt;
 		while (*next!='%'&&*next!='\0')
 			next++;
 		/* flush the string found so far */
